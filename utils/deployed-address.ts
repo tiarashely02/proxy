7716a52e00081ba8d0a079e5ff22b8bb6c93724f
tiara shely02 <tiarashely02@gmail.com>
2024-08-04 08:01:44 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x58175f685248cBF7f001cbf499Ba02235007d397'
+export const ProxyAdmin = '0x9891E08BFc6F2bcb47661914ea1a16A2c85eF3B3'
+export const SWTRImplementation = '0x2891cA9F8257336Ea0f8c9797508Fb035F85989e'
     
